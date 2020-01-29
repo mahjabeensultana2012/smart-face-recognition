e417146d04f7ad8884c53f1dc87093be5d8c8d36
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-01-28 18:54:36 -0600

    Navigation component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import Navigation from './components/Navigation/Navigation';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+      <nav>
+        <p>Sign Out</p>
+      </nav>
+    </div>
+  );
+};
+
+export default Navigation;
