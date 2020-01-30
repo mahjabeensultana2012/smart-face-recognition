3db8b80a9bb93eb083a7d83156a43f93c8bfe820
Mahjabeen Sultana <mahjabeen.sultana2012@gmail.com>
2020-01-30 12:36:49 -0600

    Implement Rank

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Rank = () => {
+  return (
+    <div>
+      <div className="f3 white">{'Nishi, your current rank is:'} </div>
+      <div className="f1 white">{'#3'}</div>
+    </div>
+  );
+};
+
+export default Rank;
